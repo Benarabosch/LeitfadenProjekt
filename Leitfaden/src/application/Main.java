2664e06fd5f5b5a57bef954482448e29f0c0ba14
bebri001 <ben.brinkmann001@stud.fh-dortmund.de>
2023-12-13 15:22:18 +0100

    First Commit

---
+++ Leitfaden/src/application/Main.java
@@ -0,0 +1,55 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			//Icon und Titel
+			stage.setTitle("Wirtschaftsinformatik Leitfaden");
+			Image icon = new Image(getClass().getResourceAsStream("/images/Icon.png"));
+			stage.getIcons().add(icon);
+			//Szene1 ist das Main Interface
+			Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+			Scene scene = new Scene(root);
+			stage.setResizable(false);
+			stage.setScene(scene);
+		
+            stage.show(); 
+            
+            // Zentriere das Fenster auf dem Bildschirm
+            centerStage(stage);
+            
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void centerStage(Stage stage) {
+        // Rufe die Bildschirmgröße ab
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+
+        // Berechne die Position für die Zentrierung
+        double stageWidth = stage.getWidth();
+        double stageHeight = stage.getHeight();
+        double x = (screenWidth - stageWidth) / 2;
+        double y = (screenHeight - stageHeight) / 2;
+
+        // Setze die berechnete Position
+        stage.setX(x);
+        stage.setY(y);
+    }
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Leitfaden/src/application/SceneController.java
@@ -0,0 +1,82 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class SceneController {
+
+	@FXML
+	Button sceneChange1;
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToScene1(ActionEvent event) {
+		try {
+			 root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+			 stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			 scene = new Scene(root);
+			 stage.setScene(scene);
+			 centerStage(stage);
+			 stage.show();
+			 
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void switchToScene2(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			centerStage(stage);
+			stage.show();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void switchToSceneLeitfaden(ActionEvent event) {
+		try {
+			 root = FXMLLoader.load(getClass().getResource("Leitfaden.fxml"));
+			 stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			 scene = new Scene(root);
+			 stage.setScene(scene);
+			 stage.show();
+			 centerStage(stage);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	private void centerStage(Stage stage) {
+        // Rufe die Bildschirmgröße ab
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+
+        // Berechne die Position für die Zentrierung
+        double stageWidth = stage.getWidth();
+        double stageHeight = stage.getHeight();
+        double x = (screenWidth - stageWidth) / 2;
+        double y = (screenHeight - stageHeight) / 2;
+
+        // Setze die berechnete Position
+        stage.setX(x);
+        stage.setY(y);
+    }
+}
