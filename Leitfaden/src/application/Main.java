08e63d056eb16ac60492c1c455cf03ba781ec8ac
Benarabosch <benbrinkmann24@gmail.com>
2023-12-16 16:36:40 +0100

    Removed comments and tested button in menu

--- Leitfaden/src/application/Main.java
+++ Leitfaden/src/application/Main.java
@@ -7,9 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-//TEST TEST TEST
+
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
